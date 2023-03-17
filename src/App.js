5a764398761853a2d1cbd673e25616db7d016263
zavalaroses <zavalaroses@gmail.com>
2023-03-17 08:03:02 -0600

    crudEmployee

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ShowEmployees from './components/ShowEmployees';
+import CreateEmployee from './components/CreateEmployee';
+import EditEmployee from './components/EditEmployee';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={ <ShowEmployees/>} />
+          <Route path='/create' element={ <CreateEmployee/>} />
+          <Route path='/edit/:id' element={ <EditEmployee/>} />
+        </Routes>
+      </BrowserRouter>        
     </div>
   );
 }
