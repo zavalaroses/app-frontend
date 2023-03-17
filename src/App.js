aceddb76db51ff89b15a125f3a2c21aa9b659fbc
ritchs <ricardohc03@gmail.com>
2023-03-17 09:25:07 -0600

    update package json

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ShowEmployees from './components/ShowEmployees';
--- src/components/CreateEmployee.js
+++ src/components/CreateEmployee.js
@@ -1,6 +1,6 @@
-import axios from 'axios'
-import React, {useState} from 'react'
-import { useNavigate } from 'react-router-dom'
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const endpoint = 'http://localhost:8000/api/employee'
 const CreateEmployee = () => {
